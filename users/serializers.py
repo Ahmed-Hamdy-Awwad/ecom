02c4d101201525c6f42152458017561234f4968f
Ahmed Hamdy <ahmed.hamdy@wyz-solutions.com>
2024-03-21 13:45:17 +0200

    Add users & orders apps

---
+++ users/serializers.py
@@ -0,0 +1,13 @@
+# from rest_framework import serializers
+
+
+# class ExampleSerializer(serializers.ModelSerializer):
+# 	class Meta:
+# 		model = yourmodel
+# 		fields = '__all__'
+
+
+# class ExampleLicenseSerializer(serializers.ModelSerializer):
+# 	class Meta:
+# 		model = yourmodel
+# 		fields = ('field1','field2')
