a76cd0e03b9a663e377573a3c9fffaa6f7e07f11
Ahmed Hamdy <ahmed.hamdy@wyz-solutions.com>
2024-03-21 14:41:52 +0200

    Add urls files

--- ecom/urls.py
+++ ecom/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
 from django.contrib import admin
+from django.urls import path, include
 from rest_framework.authtoken import views
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
+	path('users', include('users.urls')),
+	path('orders', include('orders.urls')),
 	path('login/', views.obtain_auth_token),
 ]
---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+from django.urls import include, path
+
+
+# router = routers.DefaultRouter()
+# router.register("yourpattern", yourview)
+urlpatterns = [
+	# path("", include(router.urls)),
+	# path("yourpattern", yourview.as_view()),
+]
