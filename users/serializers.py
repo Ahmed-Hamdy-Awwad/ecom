9e3c599601a87d81507cab4eea32d4cf22988d8e
ahmed <ahmed.hamdy@relianceegypt.com>
2024-03-29 16:21:38 +0200

    Add Document CRUD

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,4 @@
-from .models import Company
+from .models import *
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
@@ -27,3 +27,23 @@ class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = "__all__"
+
+
+class CreateDocumentSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Document
+		exclude = ['created_by']
+
+
+class GetDocumentSerializer(serializers.ModelSerializer):
+	company = serializers.SlugRelatedField(slug_field='name', read_only=True)
+	created_by = serializers.SlugRelatedField(slug_field='username', read_only=True)
+	class Meta:
+		model = Document
+		fields = "__all__"
+
+
+class ListDocumentSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Document
+		fields = ['id', 'name']
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
+from .views import *
 from rest_framework import routers
 from django.urls import include, path
-from .views import CompanyView, UserView
 
 
 router = routers.DefaultRouter()
 router.register("user", UserView, "user")
 router.register("company", CompanyView, "company")
+router.register("document", DocumentView, "document")
 urlpatterns = [
 	path("", include(router.urls)),
 	# path("yourpattern", yourview.as_view()),
